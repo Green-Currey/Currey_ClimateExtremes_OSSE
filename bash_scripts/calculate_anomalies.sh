e97d1999622f6881af1efc2f3dc02b7eddae256a
Bryce Currey <brycecurrey93@gmail.com>
2023-09-30 20:04:10 -0400

    add more customizability

--- bash_scripts/calculate_anomalies.sh
+++ bash_scripts/calculate_anomalies.sh
@@ -5,18 +5,21 @@ CRU="/discover/nobackup/projects/SBG-DO/data/Climate/CRU/CRU407/"
 tmp="$CRU/cru_ts4.07.1901.2022.tmp.dat.nc"
 pre="$CRU/cru_ts4.07.1901.2022.pre.dat.nc"
 out="/discover/nobackup/bcurrey/Currey_IS_ClimateExtremes_OSSE/data/"
+ref1=1970
+ref2=2010
+year=2016
 
 echo -e "Select Month"
 cdo selmon,8 $tmp $out/${month}_tmp.nc
 cdo selmon,8 $pre $out/${month}_pre.nc
 
 #select period and calculate the average through time
-cdo timmean -selyear,1970/2020 $out/${month}_tmp.nc $out/${month}_tmp_long_term_avg_1970_2020.nc
-cdo timmean -selyear,1970/2020 $out/${month}_pre.nc $out/${month}_pre_long_term_avg_1970_2020.nc
+cdo timmean -selyear,$ref1/$ref2 $out/${month}_tmp.nc $out/${month}_tmp_long_term_avg_${ref1}_${ref2}.nc
+cdo timmean -selyear,$ref1/$ref2 $out/${month}_pre.nc $out/${month}_pre_long_term_avg_${ref1}_${ref2}.nc
 
 # select 2022 and subtract that from the LTA data.
-cdo sub -selyear,2022 $out/${month}_tmp.nc  $out/${month}_tmp_long_term_avg_1970_2020.nc $out/tmp_anomaly_${month}_2022.nc
-cdo sub -selyear,2022 $out/${month}_pre.nc $out/${month}_pre_long_term_avg_1970_2020.nc $out/pre_anomaly_${month}_2022.nc
+cdo sub -selyear,$year $out/${month}_tmp.nc  $out/${month}_tmp_long_term_avg_${ref1}_${ref2}.nc $out/tmp_anomaly_${month}_${year}.nc
+cdo sub -selyear,$year $out/${month}_pre.nc $out/${month}_pre_long_term_avg_${ref1}_${ref2}.nc $out/pre_anomaly_${month}_${year}.nc
 
 rm $out/${month}_tmp.nc
 rm $out/${month}_pre.nc
